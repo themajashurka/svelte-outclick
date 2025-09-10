529da278bccaee6749249a81d40dd2327b7813e6
Marton Bene <mmartonbene@gmail.com>
2025-09-10 23:28:11 +0200

    init

---
+++ dist/OutClick.svelte.d.ts
@@ -0,0 +1,40 @@
+interface $$__sveltets_2_IsomorphicComponent<Props extends Record<string, any> = any, Events extends Record<string, any> = any, Slots extends Record<string, any> = any, Exports = {}, Bindings = string> {
+    new (options: import('svelte').ComponentConstructorOptions<Props>): import('svelte').SvelteComponent<Props, Events, Slots> & {
+        $$bindings?: Bindings;
+    } & Exports;
+    (internal: unknown, props: Props & {
+        $$events?: Events;
+        $$slots?: Slots;
+    }): Exports & {
+        $set?: any;
+        $on?: any;
+    };
+    z_$$bindings?: Bindings;
+}
+type $$__sveltets_2_PropsWithChildren<Props, Slots> = Props & (Slots extends {
+    default: any;
+} ? Props extends Record<string, never> ? any : {
+    children?: any;
+} : {});
+declare const OutClick: $$__sveltets_2_IsomorphicComponent<$$__sveltets_2_PropsWithChildren<{
+    [x: string]: any;
+    tag?: string | undefined;
+    class?: string | undefined;
+    excludeElements?: (HTMLElement | HTMLElement[]) | undefined;
+    excludeQuerySelectorAll?: string | undefined;
+    parent?: HTMLElement | undefined;
+    includeSelf?: boolean | undefined;
+    halfClick?: boolean | undefined;
+}, {
+    default: {};
+}>, {
+    outclick: CustomEvent<{
+        target: HTMLElement;
+    }>;
+} & {
+    [evt: string]: CustomEvent<any>;
+}, {
+    default: {};
+}, {}, string>;
+type OutClick = InstanceType<typeof OutClick>;
+export default OutClick;
